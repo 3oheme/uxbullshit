7362481a3371805594b5320dda6c1851affad066
3oheme <bohemegm@gmail.com>
2014-02-10 09:22:40 +0000

    @3oheme moar bullshit, dude

--- bullshit.js
+++ bullshit.js
@@ -89,9 +89,16 @@ function ux_people() {
 	return random([
 		"Jakob Nielsen",
 		"Don Norman",
+    "Steve Jobs",
 		"Alan Cooper",
+    "David Carson",
 		"Bill Buxton",
     "John Ive",
+    "Douglas Adams",
+    "Albert Einstein",
+    "Paul Graham",
+    "Jef Raskin",
+    "Tim Berners-Lee",
 	]);
 }
 
@@ -125,7 +132,9 @@ function ux_bullshit() {
 		company() + " is killing " + ux_technique(),
 		"You should use " + adjective() +" " + ux_technique() + " to " + outcome(),
 		"Don't trust " + ux_people() + " and use a " + adjective() +" " + ux_technique() + " if you need to " + outcome(),
+		"Follow  " + ux_people() + " and " + outcome() + " using a" + adjective() +" " + adjective() + " " + ux_technique(),
     "To " + outcome() + " try thinking in a " + adjective() +" " + ux_technique(),
+    "A wise man would use a " + adjective() +" " + adjective() + " " + ux_technique() + " to " + outcome(),
  	];
 	return result[Math.floor(Math.random()*result.length)];
 }
