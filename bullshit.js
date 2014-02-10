fde81c1c176047121cf8adbd23d3d77dddb8781c
3oheme <bohemegm@gmail.com>
2014-02-10 11:02:56 +0000

    @3oheme fixed an unspaced bullshit

--- bullshit.js
+++ bullshit.js
@@ -132,7 +132,7 @@ function ux_bullshit() {
 		company() + " is killing " + ux_technique(),
 		"You should use " + adjective() +" " + ux_technique() + " to " + outcome(),
 		"Don't trust " + ux_people() + " and use a " + adjective() +" " + ux_technique() + " if you need to " + outcome(),
-		"Follow  " + ux_people() + " and " + outcome() + " using a" + adjective() +" " + adjective() + " " + ux_technique(),
+		"Follow  " + ux_people() + " and " + outcome() + " using a " + adjective() +" " + adjective() + " " + ux_technique(),
     "To " + outcome() + " try thinking in a " + adjective() +" " + ux_technique(),
     "A wise man would use a " + adjective() +" " + adjective() + " " + ux_technique() + " to " + outcome(),
  	];
