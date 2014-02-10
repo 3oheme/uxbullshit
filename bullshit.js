31d4f839250bb98d974868ddca45fd7d4ccbdf64
3oheme <ignacio.palomo.duarte@gmail.com>
2014-02-10 00:18:00 +0000

    @3oheme added tweet this bullshit or get another one links

--- bullshit.js
+++ bullshit.js
@@ -130,8 +130,19 @@ function ux_bullshit() {
 	return result[Math.floor(Math.random()*result.length)];
 }
 
-var item = document.getElementById("technique");
+
 setTimeout(function(){
-	item.textContent = '“' + ux_bullshit() + '”';
+	magic_ux_shit = ux_bullshit()
+	var item = document.getElementById("technique");
+	item.textContent = '“' + magic_ux_shit + '”';
+	
+	var more = document.getElementById("more-info");
+	more.removeAttribute("style");
+	
+	var tweet = document.getElementById("tweet");
+	var technique_encoded = encodeURIComponent(magic_ux_shit + " — via @uxbullshit");
+	var url_encoded = encodeURIComponent('http://uxbullshit.com')
+	var tweet_link = 'https://twitter.com/intent/tweet?text=' + technique_encoded + "&url=" + url_encoded;
+	tweet.href = tweet_link
 }, 2000);
 
