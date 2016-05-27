66788ab74d124020f7064001647d21be3ed902c2
Ignacio Palomo Duarte <ignacio@spotahome.com>
2016-05-27 16:33:48 +0200

    Added nlp-compromise and reordered sentence

--- ux_bullshit.js
+++ ux_bullshit.js
@@ -1,17 +1,76 @@
 function ux_bullshit() {
-	var result = [
-		"You should try a " + adjective() +" " + ux_technique() + " with a " + role() + " to " + outcome(),
-		"Go and fire a " + role(),
-		"Think in " + ux_technique() + " as the new " + ux_technique(),
-		company() + " is killing " + ux_technique(),
-		"You should use " + adjective() +" " + ux_technique() + " to " + outcome(),
-		"Don't trust " + ux_people() + " and use a " + adjective() +" " + ux_technique() + " if you need to " + outcome(),
-		"Follow " + ux_people() + " and " + outcome() + " using a " + adjective() +" " + adjective() + " " + ux_technique(),
-	    "To " + outcome() + " try thinking in a " + adjective() +" " + ux_technique(),
-	    "A wise man would use a " + adjective() + " " + adjective() + " " + ux_technique() + " to " + outcome(),
-	    "Think outside the box to " + outcome() + " and " + outcome(),
-	    "You can simply " + outcome() + " applying a " + adjective() +" " + ux_technique(),
-	    "Stop using a " + adjective() +' '+ adjective() +" " + ux_technique() + " if you need to " + outcome(),
- 	];
-	return result[Math.floor(Math.random()*result.length)];
+
+    var allSentences = [];
+
+    allSentences.push(
+        function() {
+            return "You should try a " + adjective() +" " + ux_technique() + " with a " + role() + " to " + outcome();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "Go and fire a " + role();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "Think in " + ux_technique() + " as the new " + ux_technique();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return company() + " is killing " + ux_technique();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "You should use " + adjective() +" " + ux_technique() + " to " + outcome();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "Don't trust " + ux_people() + " and use a " + adjective() +" " + ux_technique() + " if you need to " + outcome();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "Follow " + ux_people() + " and " + outcome() + " using a " + adjective() +" " + adjective() + " " + ux_technique();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "To " + outcome() + " try thinking in a " + adjective() +" " + ux_technique();
+        }
+    );
+
+    allSentences.push(
+        function() {
+            return "A wise man would use a " + adjective() + " " + adjective() + " " + ux_technique() + " to " + outcome();
+        }
+    );
+    allSentences.push(
+        function() {
+            return "Think outside the box to " + outcome() + " and " + outcome();
+        }
+    );
+    allSentences.push(
+        function() {
+            return "You can simply " + outcome() + " applying a " + adjective() +" " + ux_technique();
+        }
+    );
+    allSentences.push(
+        function() {
+            return "Stop using a " + adjective() +' '+ adjective() +" " + ux_technique() + " if you need to " + outcome();
+        }
+    );
+
+    return random(allSentences)();
+
 }
