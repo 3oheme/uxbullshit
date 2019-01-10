30dc946269305d02e64b614eed83dc75be14036e
Palomo Duarte <bohemegm@gmail.com>
2019-01-10 16:39:41 +0100

    Update init.js

--- init.js
+++ init.js
@@ -2,14 +2,14 @@
 	magic_ux_shit = ux_bullshit()
 	var item = document.getElementById("technique");
 	item.textContent = '“' + magic_ux_shit + '”';
-	
+
 	var more = document.getElementById("more-info");
 	more.removeAttribute("style");
-	
+
 	var tweet = document.getElementById("tweet");
 	var technique_encoded = encodeURIComponent(magic_ux_shit + " — via @uxbullshit");
 	var url_encoded = encodeURIComponent('http://uxbullshit.com')
 	var tweet_link = 'https://twitter.com/intent/tweet?text=' + technique_encoded + "&url=" + url_encoded;
 	tweet.href = tweet_link
-}, 1000);
+}, 1100);
 
